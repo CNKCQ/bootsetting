c23b7efa028af3cbba9f3461a2fb8f724f0e6dc6
steve <chengquan.wang@ele.me>
2018-07-11 21:24:33 +0800

    first commit

---
+++ lib/bootsetting.rb
@@ -0,0 +1,15 @@
+require "bootsetting/version"
+
+module Bootsetting
+  # Your code goes here...
+  puts 'Hello gem'
+		class Helper
+				def self.portray(food)
+						if food.downcase == "broccoli"
+										"Gross!"
+						else
+										"Delicious!"
+						end
+				end
+		end
+end
---
+++ lib/bootsetting/version.rb
@@ -0,0 +1,3 @@
+module Bootsetting
+  VERSION = "0.1.0"
+end
