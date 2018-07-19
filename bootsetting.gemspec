09510284b3be16ac3f154a87c23d626aca4ec7fc
steve <chengquan.wang@ele.me>
2018-07-19 21:39:04 +0800

    add: command line tool --- Thor

--- bootsetting.gemspec
+++ bootsetting.gemspec
@@ -35,7 +35,13 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
  	spec.add_runtime_dependency 'colored2'
+ 	spec.add_runtime_dependency 'rainbow'
+ 	spec.add_runtime_dependency 'thor'
 
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
+  spec.add_development_dependency "rainbow"
+  spec.add_development_dependency "thor"
+
+
 end
--- lib/bootsetting.rb
+++ lib/bootsetting.rb
@@ -1,8 +1,27 @@
 require "bootsetting/version"
+require 'rainbow/refinement'
+require "thor"
+using Rainbow
 
 module Bootsetting
   # Your code goes here...
-  puts 'Hello gem, you are best beauty, maybe something has change'
-  `hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x7000000E0},{"HIDKeyboardModifierMappingSrc":0x7000000E0,"HIDKeyboardModifierMappingDst":0x700000039}]}'`
-  exit
+class CLI < Thor
+  # contents of the Thor class
+  desc "hello NAME", "say hello to NAME"
+  def hello(name, from=nil)
+    puts "from: #{from}" if from
+    puts "Hello method #{name}".bright.red
+  end
+
+  desc "UserKeyMapping", "say hello to new keyMapping"
+  def kswitch()
+		`hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x7000000E0},{"HIDKeyboardModifierMappingSrc":0x7000000E0,"HIDKeyboardModifierMappingDst":0x700000039}]}'`
+		puts "say hello to new keyMapping".bright.green
+  end
 end
+
+CLI.start(ARGV)
+exit
+end
+
+# Thor See: http://whatisthor.com/
